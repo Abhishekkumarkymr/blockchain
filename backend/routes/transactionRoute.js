2068673dc7a4bd2bce9890379db95fccbe0bca37
Abhishekkumarkymr <166300342+Abhishekkumarkymr@users.noreply.github.com>
2024-05-12 11:41:37 +0530

    Delete backend directory

--- backend/routes/transactionRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { createTransaction } = require("../controller/createTransaction");
-const { getTransaction } = require("../controller/getTransaction");
-router.post("/createTransaction",createTransaction );
-router.get("/getAllTransaction", getTransaction);
-
-module.exports = router;
